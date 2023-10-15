4905e065f53ee83fad187866700c0d71958351e3
lbu0413 <vvookly@gmail.com>
2023-10-15 02:44:03 -0400

    10-15-23

--- ps_with_python/section5/교육과정설계.py
+++ ps_with_python/section5/교육과정설계.py
@@ -10,17 +10,13 @@ for i in range(N):
     classes = list(input())
     Q = deque(input_str)
     for x in classes:
-        if len(Q) == 0:
-            print(f"#{i+1} YES")
-            break
-        if x == Q[0]:
-            Q.popleft()
-        else:
-            if x in Q:
+        if x in Q:
+            if x != Q.popleft():
                 print(f"#{i+1} NO")
                 break
+
     else:
-        if len(Q) != 0:
-            print(f"#{i+1} NO")
-        else:
+        if len(Q) == 0:
             print(f"#{i+1} YES")
+        else:
+            print(f"#{i+1} NO")
